84066b972f835615356f04a10d57a547c421fcf7
djarrel <jeffdarrellthegreat@gmail.com>
2020-01-30 15:16:53 +0300

    index.html

---
+++ notes.apps.js
@@ -0,0 +1,33 @@
+const notes = [{
+    title: 'my next trip',
+    body:'I would like to go to spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat',
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p');
+        noteEl.textContent = note.title;
+        document.querySelector(`#notes`).appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters);
+
+document.querySelector(`#search-text`).addEventListener(`input`,
+function (e){
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters)
+})
